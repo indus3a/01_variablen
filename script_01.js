728d3b46752d9aa7d19d908d9754579457b063ff
indus3a-local <pvi@pm.me>
2021-02-08 12:07:03 +0100

    types

--- script_01.js
+++ script_01.js
@@ -2,3 +2,22 @@
 
 // console.log("hallo");
 // console.log("hi");
+/*
+let firstName = "Max";
+let familyName = "Mutze";
+console.log(firstName);
+console.log(familyName);
+console.log (firstName + " " + familyName);
+
+let firstName,familyName;
+firstName = prompt("Bitte Vorname eingeben!");
+familyName = prompt("Bitte Nachname eingeben!");
+console.log("Hallo, " + firstName + " " + familyName);
+*/
+
+let test;
+test = "hi";
+//test = 2;
+//test = true;
+console.log("Typ: " + typeof test);
+console.log("inhalt: " + test);
